6ace7dd038304f0c4ff32757ed1a24942d94ece2
nontangent <irregular0number@gmail.com>
2021-01-24 17:03:08 +0900

    2 files modified

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,36 +1,36 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
-import { AppService } from 'src/app/app.service';
+import { Component, ElementRef, OnInit } from "@angular/core";
+import { AppService } from "../../app.service";
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.scss"]
 })
 export class ChildComponent implements OnInit {
-
-  constructor(
-    public app: AppService,
-    private el: ElementRef
-  ) { }
+  constructor(public app: AppService, private el: ElementRef) {}
 
   ngOnInit(): void {
-    this.app.setElement('app-child', this.el);
+    this.app.setElement("app-child", this.el);
   }
 
   get sidePadding(): number {
-    const style = getComputedStyle(this.app.getElement('app-child').nativeElement);
-    const value = parseFloat(style.getPropertyValue('--side-padding'));
+    const style = getComputedStyle(
+      this.app.getElement("app-child").nativeElement
+    );
+    const value = parseFloat(style.getPropertyValue("--side-padding"));
     return value;
   }
 
   get parentSidePadding(): number {
-    const style = getComputedStyle(this.app.getElement('app-parent').nativeElement);
-    const value = parseFloat(style.getPropertyValue('--side-padding'));
+    const style = getComputedStyle(
+      this.app.getElement("app-parent").nativeElement
+    );
+    const value = parseFloat(style.getPropertyValue("--side-padding"));
     return value;
   }
 
   changeSidePadding(selector: string, px: number) {
     const style = this.app.getElement(selector).nativeElement.style;
-    style.setProperty('--side-padding', `${px}px`);
+    style.setProperty("--side-padding", `${px}px`);
   }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,26 +1,23 @@
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
-import { AppService } from 'src/app/app.service';
+import { AfterViewInit, Component, ElementRef, OnInit } from "@angular/core";
+import { AppService } from "../../app.service";
 
 @Component({
-  selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.scss']
+  selector: "app-parent",
+  templateUrl: "./parent.component.html",
+  styleUrls: ["./parent.component.scss"]
 })
 export class ParentComponent implements OnInit {
-
-  constructor(
-    public app: AppService,
-    private el: ElementRef
-  ) { }
+  constructor(public app: AppService, private el: ElementRef) {}
 
   ngOnInit(): void {
-    this.app.setElement('app-parent', this.el);
+    this.app.setElement("app-parent", this.el);
   }
 
   get sidePadding(): number {
-    const style = getComputedStyle(this.app.getElement('app-parent').nativeElement);
-    const value = parseFloat(style.getPropertyValue('--side-padding'));
+    const style = getComputedStyle(
+      this.app.getElement("app-parent").nativeElement
+    );
+    const value = parseFloat(style.getPropertyValue("--side-padding"));
     return value;
   }
-
 }
